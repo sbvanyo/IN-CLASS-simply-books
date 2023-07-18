f61b725bbe09cdd44271360e94ff5cda09bbd09a
Stacey Vanyo <sbvanyo@gmail.com>
2023-07-17 20:37:00 -0500

    pushing so I can submit a ticket

--- pages/author/[firebaseKey].js
+++ pages/author/[firebaseKey].js
@@ -19,7 +19,7 @@ export default function ViewAuthor() {
   return (
     <div className="mt-5 d-flex flex-wrap">
       <div className="d-flex flex-column">
-        <img src={authorDetails.image} alt={authorDetails.first_name + authorDetails.last_name} style={{ width: '300px' }} />
+        {/* <img src={authorDetails.image} alt={authorDetails.first_name + authorDetails.last_name} style={{ width: '300px' }} /> */}
       </div>
       <div className="text-white ms-5 details">
         <h5>
